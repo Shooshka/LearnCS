920d566c6d849c9d959f002c9d62a5e95e92cfb5
Shoo <root@ishoo.ru>
2017-09-21 15:21:47 +0300

    fff

--- Learn1/Learn1/Program.cs
+++ Learn1/Learn1/Program.cs
@@ -10,7 +10,27 @@ namespace Learn1
     {
         static void Main(string[] args)
         {
-            int a = 5; //коммент
+            //В Си-шарп объявление массива имеет такую структуру:
+
+            //тип[] имя_массива = new тип[размер массива];
+
+            //int[] numbers = new int[5]; // Создаем массив целых чисел
+            //string[] seasons = new string[4] { "зима", "весна", "лето", "осень" }; // Создаем массив строк, инициализируем его значениями.
+            // Также корректно пропускать new etc:
+            //string[] seasons = { "зима", "весна", "лето", "осень" };
+            //numbers[0] = 5;
+            //Console.WriteLine(numbers[0] + " " + seasons[0]);
+            //Console.ReadKey();
+
+            //Многомерные
+
+            int[,] numbers1 = new int[2, 2]; //Объявление двумерного массива
+            int[,,] numbers2 = new int[2, 2, 3]; //Объявление трехмерного массива
+            int[,] numbers3 = new int[3, 2] { { 6, 0 }, { 5, 7 }, { 8, 9 } }; //Объявление и иинициализация двумерного массива
+
+            Console.WriteLine(Convert.ToInt16(numbers1.Length));
+            Console.ReadKey();
+            
         }
     }
 }
