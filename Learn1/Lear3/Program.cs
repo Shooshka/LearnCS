dc2d1f370f1ab90853f0cb6352e1968bdf294413
Shoo <root@ishoo.ru>
2017-09-25 20:24:44 +0300

    foreach

---
+++ Learn1/Lear3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Цикл foreach
+
+            /*
+            int[] numbers = { 1, 564, 234, 567, 234, 76 };
+            int s = 0;
+
+            foreach (int el in numbers)
+            {
+                s += el;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+            */
+
+            //Homework
+
+            //Используя цикл foreach, выведите на экран все элементы массива целых чисел, которые больше 20 и меньше 50.
+            int[] numbers = { 1, 33, 564, 234, 48, 567, 234, 76 };
+            foreach (int el in numbers)
+            {
+                if (el > 20 && el < 50)
+                {
+                    Console.WriteLine(el);
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
