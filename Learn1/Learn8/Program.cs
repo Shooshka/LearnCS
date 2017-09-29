02ad982d164902fed6896a6b5d13b325eef80362
Shoo <root@ishoo.ru>
2017-09-29 21:33:19 +0300

    dicts

---
+++ Learn1/Learn8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learn8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Словари
+
+            //Dictionaries are special lists, whereas every value in the list has a key which is also a variable.A good example for a dictionary is a phone book.
+
+            //Dictionary<string, long> phonebook = new Dictionary<string, long> { }; //Создаем пустой
+            //phonebook.Add("Alex", 89261112233); //Добавляем
+            //phonebook["Me"] = 89262223344; //Или так
+            //Console.WriteLine("Телефон Alex'a - " + phonebook["Alex"]);
+            //if (phonebook.ContainsKey("Me")) //Проверяем, есть ли такой в словаре ключ
+            //{
+            //    Console.WriteLine("Телефон мой - " + phonebook["Me"]);
+            //}
+
+            //phonebook.Remove("Me"); //Удаляем из словаря
+            //Console.WriteLine(phonebook.Count); //Проверяем, что остался только 1 элемент
+
+            //Console.ReadKey();
+
+            //Exercise
+            //Create a new dictionary called inventory that holds 3 names of fruits, and the amount they are in stock.
+
+            //Here is the inventory specification:
+
+            //3 of type apple
+            //5 of type orange
+            //2 of type banana
+
+            //Dictionary<string, long> inventory = new Dictionary<string, long> { };
+            //inventory["apple"] = 3;
+            //inventory["orange"] = 5;
+            //inventory["banana"] = 2;
+
+            //Console.WriteLine(inventory["apple"]);
+            //Console.WriteLine(inventory["orange"]);
+            //Console.WriteLine(inventory["banana"]);
+            //Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
